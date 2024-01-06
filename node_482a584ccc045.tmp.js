69cd53030455fcc967c632c063a161b57a97e343
kakk2093 <80924040+kakk2093@users.noreply.github.com>
2024-01-06 16:35:40 +0400

    Create node_482a584ccc045.tmp.js

---
+++ node_482a584ccc045.tmp.js
@@ -0,0 +1,24 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", data => message(data, ws)); 
+ // ws.on("close", close());
+
+  ws.send("something");
+});
+
+function close() {
+    console.log('client disconect');
+}
+
+function message(data, ws) {
+    console.log("received: ", data);
+    ws.send(data);
+}
+
+
+
+
+
